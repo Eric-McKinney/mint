2c017a7ed2f11c7c6b265c9daf9bf5083e30cd10
Eric-McKinney <ericmckinney.md@gmail.com>
2024-04-19 23:15:27 -0400

    Initial commit

---
+++ src/lexer.h
@@ -0,0 +1,32 @@
+#ifndef Lexer_h
+#define Lexer_h
+
+typedef enum {
+    TOK_LPAREN,
+    TOK_RPAREN,
+    TOK_DOT,
+    TOK_EQUAL,
+    TOK_ADD,
+    TOK_SUB,
+    TOK_MULT,
+    TOK_DIV,
+    TOK_FUN,
+    TOK_ENDLN,
+    TOK_INT,
+    TOK_FLOAT,
+    TOK_ID
+} Tok_t;
+
+typedef struct token {
+    Tok_t token;
+    union {
+        int i;
+        double d;
+        char *id;
+    } value;
+    token *next;
+} TokenList;
+
+TokenList *tokenize(char **input);
+
+#endif
