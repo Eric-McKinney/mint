ec3809a12217b4fc6ae103a78d9b572ab373a7d5
Eric-McKinney <ericmckinney.md@gmail.com>
2024-04-24 23:20:40 -0400

    Added prototype for token_list_to_str

--- src/lexer.h
+++ src/lexer.h
@@ -30,5 +30,6 @@ typedef struct token {
 TokenList *tokenize(const char *input);
 void free_token_list(TokenList *tok_l);
 void print_token_list(TokenList *tok_l);
+char *token_list_to_str(TokenList *tok_l);
 
 #endif
