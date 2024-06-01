6641c14389d9f8b22dc247e8b8575137b01b192a
Eric-McKinney <ericmckinney.md@gmail.com>
2024-06-01 13:56:29 -0400

    Initial commit

---
+++ tests/eval_tests.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "../src/eval.h"
+
+typedef struct {
+    const char *name;
+    ExprTree *input;
+    const char *ans;
+}
+
+int main(int argc, char **argv) {
+    return 0;
+}
