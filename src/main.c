bab07294eff02b22d875b0a9a13a92ce13a078d2
Eric-McKinney <ericmckinney.md@gmail.com>
2024-10-07 12:56:26 -0400

    Switched to readline for repl

--- src/main.c
+++ src/main.c
@@ -14,46 +14,85 @@
 #define PROMPT TEAL "mint" END_COLOR "|> "
 #define BUF_SIZE 1024
 
-static void process_cmd(char *cmd, Env_t *env);
+static char *process_input(char *input, Env_t *env);
+static char *aggregate_args(int argc, char **argv);
+static void process_file(FILE *file, Env_t *env);
+static void repl_loop(Env_t *env);
 
 int main(int argc, char **argv) {
     Env_t *env = init_env();
+    compile_regexs();
 
     /* determine invocation method */
     if (argc == 1) {
         if (isatty(0)) {
             repl_loop(env);
         } else {
+            /* input redirection */
             process_file(stdin, env);
         }
     } else if (argc > 1) {
+        char *expr, *result;
+
         /* process args as expr */
+        expr = aggregate_args(argc, argv);
+        result = process_input(expr, env);
+
+        if (result != NULL) {
+            printf("%s\n", result);
+        }
+
+        free(expr);
+        free(result);
     }
 
     free_env(env);
+    free_regexs();
     return 0;
 }
 
+static char *aggregate_args(int argc, char **argv) {
+    int i, expr_length = 0;
+    char *expr;
+
+    for (i = 1; i < argc; i++) {
+        expr_length += strlen(argv[i]) + 1; /* appending a space */
+    }
+
+    expr = malloc(expr_length + 1);
+
+    for (i = 1; i < argc; i++) {
+        strcat(expr, argv[i]);
+        strcat(expr, " ");
+    }
+
+    return expr;
+}
+
 static void process_file(FILE *file, Env_t *env) {
-    char line[BUF_SIZE] = {0};
+    char line[BUF_SIZE] = {0}, *result = NULL;
 
     while(fgets(line, BUF_SIZE, file)) {
-        char cmd[BUF_SIZE] = "";
+        free(result);
+        line[strlen(line) - 1] = '\0';
 
-        sscanf(line, "%s", cmd);
-
-        if (strcmp(cmd, "") == 0) {
+        if (strcmp(line, "") == 0) {
             continue;
         }
 
-        process_cmd(line, env);
+        result = process_input(line, env);
+    }
+    
+    if (result != NULL) {
+        printf("%s\n", result);
     }
 
+    free(result);
     fclose(file);
 }
 
 static void repl_loop(Env_t *env) {
-    char *line;
+    char *line, *result;
 
     while ((line = readline(PROMPT))) {
         if (strcmp(line, "") == 0) {
@@ -64,22 +103,28 @@ static void repl_loop(Env_t *env) {
             break;
         }
 
-        process_cmd(line, env);
+        result = process_input(line, env);
+
+        if (result != NULL) {
+            printf("%s\n", result);
+        }
+
         add_history(line);
+        free(result);
     }
 }
 
-static void process_cmd(char *cmd, Env_t *env) {
-    char *str;
+static char *process_input(char *input, Env_t *env) {
     TokenList *tok_l;
     ExprTree *tree;
+    char *result;
 
     errno = 0;
-    tok_l = tokenize(cmd);
+    tok_l = tokenize(input);
 
     if (errno != 0) {
         free_token_list(tok_l);
-        return;
+        return NULL;
     }
 
     tree = parse(tok_l);
@@ -87,7 +132,7 @@ static void process_cmd(char *cmd, Env_t *env) {
     if (errno != 0) {
         free_token_list(tok_l);
         free_expr_tree(tree);
-        return;
+        return NULL;
     }
     
     eval(&tree, env);
@@ -95,16 +140,13 @@ static void process_cmd(char *cmd, Env_t *env) {
     if (errno != 0) {
         free_token_list(tok_l);
         free_expr_tree(tree);
-        return;
+        return NULL;
     }
 
-    str = eval_result_to_str(tree);
+    result = eval_result_to_str(tree);
 
-    if (strlen(str) > 0) {
-        printf("%s\n", str);
-    }
-
-    free(str);
     free_token_list(tok_l);
     free_expr_tree(tree);
+
+    return result;
 }
